b683d4fc7ba183a31892d2d1f1d7d20687b16563
TomoKauf <tomaszkauf@gmail.com>
2021-02-10 21:32:51 +0100

    111

---
+++ zegarek.py
@@ -0,0 +1,21 @@
+import time
+import easygui
+import winsound
+
+a = 1
+b = easygui.integerbox ("przez ile sekund pokazywac zegar")
+c = b
+
+while (a <= b):
+ lokczas= time.localtime()
+ czas = time.strftime("%H:%M:%S", lokczas)
+ print (czas, "pozostalo jeszcze",c,"sekund")
+ time.sleep (1)
+ a = a + 1
+ c = c - 1
+ if c == 0:
+  print ('KONIEC')
+  #sygnal dzwiekowy potrzebny winsound (zaimportowany powyzej) pierwszy paramater to czestotliwosc, drugi dlugosc w ms
+  winsound.Beep(2500,1000)
+  time.sleep (5)
+
